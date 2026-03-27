dd2975075c6be67f94f39082fdc55a45f843d895
George Panousakis <gpanouss@hotmail.com>
2026-03-27 15:04:44 +0200

    Added "el" language for testing

--- tests/api/test_user_preferences_language.py
+++ tests/api/test_user_preferences_language.py
@@ -245,7 +245,7 @@ class TestUserPreferencesLanguage:
         self, client: TestClient, db_session: Session
     ):
         """Test that all supported languages can be set successfully."""
-        supported_languages = ["en", "fr", "de", "es", "it", "pt", "ru", "sv", "nl", "pl"]
+        supported_languages = ["en", "el", "fr", "de", "es", "it", "pt", "ru", "sv", "nl", "pl"]
 
         for lang in supported_languages:
             # Create a new test user for each language
